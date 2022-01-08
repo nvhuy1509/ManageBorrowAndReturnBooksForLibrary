4e6e6699bbcf844be13f12352508eba367b09b19
huyvan99 <59367510+huyvan99@users.noreply.github.com>
2022-01-09 00:23:12 +0700

    update

--- app/view/login_view.php
+++ app/view/login_view.php
@@ -25,7 +25,7 @@
                 <br/>
                 <input type="submit" name="btn_submit" value="Đăng nhập"/>
             </form>
-            <a class="btn btn-default" title="Forget Password" href="index.php?url=userRS&method=getView">Forget Password</a>
+            <a class="btn btn-default" title="Forget Password" href="index.php?url=userRS&method=getView">Quên Password</a>
         </div>
 	</div>
 </body>
